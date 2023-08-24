50b014faba106404019e38766958feb31f16dea3
justSmK <28645140+justSmK@users.noreply.github.com>
2023-08-24 14:40:55 +0300

    Project init

---
+++ Avito Tech Assignment/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Avito Tech Assignment
+//
+//  Created by Sergei Semko on 8/24/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
